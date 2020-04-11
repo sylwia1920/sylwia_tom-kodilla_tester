3f842e77ad2d564a892d807241fc8fcdbc44f747
sylwia1920 <sylwia1920@gmail.com>
2020-04-11 14:39:06 +0200

    Projects

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
@@ -7,17 +7,17 @@ public class FlightRepository {
 
         public static List<Flight> getFlightTable() {
             List<Flight> flights = new ArrayList<>();
-            flights.add(new Flight("Kraków", "Moskwa"));
-            flights.add(new Flight("Kraków", "Warszawa"));
-            flights.add(new Flight("Kraków", "Paryż"));
-            flights.add(new Flight("Kraków", "Rzym"));
-            flights.add(new Flight("Londyn", "Oslo"));
-            flights.add(new Flight("Praga", "Berlin"));
-            flights.add(new Flight("Katowice", "Madryt"));
-            flights.add(new Flight("Warszawa", "Los Angeles"));
-            flights.add(new Flight("Pekin", "Ottawa"));
-            flights.add(new Flight("Detroit", "Warszawa"));
-            flights.add(new Flight("Grańsk", "Kraków"));
+            flights.add(new Flight("Warsaw", "Paris"));
+            flights.add(new Flight("Lodz", "Ankara"));
+            flights.add(new Flight("Wroclaw", "Rome"));
+            flights.add(new Flight("Warsaw", "Oslo"));
+            flights.add(new Flight("Sopot", "London"));
+            flights.add(new Flight("Warsaw", "Praha"));
+            flights.add(new Flight("Poznan", "Madryt"));
+            flights.add(new Flight("Warsaw", "New York"));
+            flights.add(new Flight("Lodz", "Tokyo"));
+            flights.add(new Flight("Pila", "Kair"));
+            flights.add(new Flight("Gdansk", "Berlin"));
             return flights;
         }
     }
