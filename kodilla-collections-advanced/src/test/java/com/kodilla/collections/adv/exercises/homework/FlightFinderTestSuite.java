9c6eccb85841e7e489a8cafc100fc4432c270537
sylwia1920 <sylwia1920@gmail.com>
2020-04-15 01:55:39 +0200

    Projects

---
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightFinderTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FlightFinderTestSuite {
+
+        @Test
+        public void testFindFlightsFrom() {
+            FlightFinder flightFinder = new FlightFinder();
+            flightFinder.findFlightsFrom("Kraków");
+
+
+            assertEquals(4, flightFinder.findFlightsFrom("Kraków").size());
+        }
+    }
