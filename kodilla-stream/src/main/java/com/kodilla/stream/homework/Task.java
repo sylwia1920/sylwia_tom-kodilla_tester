8baf6eaced856047bc2133d55e0ee4096bfee17b
sylwia1920 <sylwia1920@gmail.com>
2020-04-15 01:47:03 +0200

    Projects

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/Task.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/Task.java
@@ -1,10 +1,30 @@
 package com.kodilla.stream.homework;
 
+import java.time.LocalDate;
+
 public class Task {
     private String name;
-    private int localDateOpened;
-    private int localDateDeadline;
+    private LocalDate opened;
+    private LocalDate deadline;
 
+    public Task(String name, LocalDate opened, LocalDate deadline) {
+        this.name = name;
+        this.opened = opened;
+        this.deadline = deadline;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getOpened() {
+        return opened;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
 
 
 }
